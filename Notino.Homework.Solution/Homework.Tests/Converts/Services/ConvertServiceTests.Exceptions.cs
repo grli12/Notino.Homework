39e96c60e4b69f92955ae644eea027f06fe513e1
grlicky.martin@gmail.com <grlicky.martin@gmail.com>
2022-05-10 22:24:23 +0200

    ShouldThrowUnsupportedConvertExceptionOnConvertToWhenConvertAdapterNotFoundExceptionIsThrown -> FAIL

--- Notino.Homework.Solution/Homework.Tests/Converts/Services/ConvertServiceTests.Exceptions.cs
+++ Notino.Homework.Solution/Homework.Tests/Converts/Services/ConvertServiceTests.Exceptions.cs
@@ -47,5 +47,41 @@ namespace Homework.Tests.Converts.Services
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.convertAdapterResolverMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowUnsupportedConvertExceptionOnConvertToWhenConvertAdapterNotFoundExceptionIsThrown()
+        {
+            //given;
+            string keyTo = "keyTo";
+
+            var convertAdapterNotFoundException =
+                new ConvertAdapterNotFoundException(keyTo);
+
+            var expectedUnsupportedConvertException =
+                new UnsupportedConvertException(convertAdapterNotFoundException);
+
+            this.convertAdapterResolverMock.Setup(resolver =>
+                resolver.Resolve(keyTo))
+                    .Throws(convertAdapterNotFoundException);
+
+            //when
+            await Assert.ThrowsAsync<UnsupportedConvertException>(() =>
+                this.convertService.ConvertAsync(It.IsAny<string>(),keyTo, It.IsAny<byte[]>(), It.IsAny<string>()));
+
+            //then
+
+            this.loggingBrokerMock.Verify(logginBroker =>
+                logginBroker.LogError(It.Is(SameExceptionAs(
+                    expectedUnsupportedConvertException))),
+                        Times.Once());
+
+            this.convertAdapterResolverMock.Verify(resolver =>
+                resolver.Resolve(keyTo),
+                    Times.Exactly(2));
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.convertAdapterResolverMock.VerifyNoOtherCalls();
+        }
     }
 }
