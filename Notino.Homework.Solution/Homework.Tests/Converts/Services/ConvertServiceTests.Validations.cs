d740771d801119284eace418389aca0ecc329a0f
grlicky.martin@gmail.com <grlicky.martin@gmail.com>
2022-05-11 04:14:00 +0200

    ShouldThrowValidationExceptionOnConvertWhenConvertKeyToIsInvalidAndLogItAsync -> FAIL

--- Notino.Homework.Solution/Homework.Tests/Converts/Services/ConvertServiceTests.Validations.cs
+++ Notino.Homework.Solution/Homework.Tests/Converts/Services/ConvertServiceTests.Validations.cs
@@ -41,5 +41,35 @@ namespace Homework.Tests.Converts.Services
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnConvertWhenConvertKeyToIsInvalidAndLogItAsync(
+            string invalidConvertKey)
+        {
+            string validConvertKey = GetRandomText();
+            byte[] validData = new byte[10];
+            string validPath = GetRandomText();
+            //given
+            var adapterKeyValidationException =
+                new AdapterKeyValidationException();
+
+            var expectedConvertValidationException =
+                new ConvertValidationException(adapterKeyValidationException);
+
+            //when
+            await Assert.ThrowsAsync<ConvertValidationException>(() =>
+                this.convertService.ConvertAsync(validConvertKey, invalidConvertKey, validData, validPath));
+
+            //then
+            this.loggingBrokerMock.Verify(logging =>
+                logging.LogError(It.Is(SameExceptionAs(expectedConvertValidationException))));
+
+            this.convertAdapterResolverMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
